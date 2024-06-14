d95de245df33bb1cf056bd9081bfccc308c45fdb
Gowtham <gajavijay4@gmail.com>
2024-06-14 23:31:36 +0530

    Initialized Tailwindcss and modified README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Hello form react</h1>
+    <h1 className='text-green-400'>Hello form react</h1>
     </>
 
   )
