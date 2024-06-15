ac1c0622ef703ccfe647de3c8073aa92e7191bd2
Gowtham <gajavijay4@gmail.com>
2024-06-15 13:39:24 +0530

    gitLink

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   <>
   <footer className="w-full h-10 absolute bottom-0 left-0 mt-10">
     <nav className="w-full h-full flex justify-center items-center">
-        Built By 	&nbsp; <a href="https://github.com.Gowtz" className="text-blue-500"> @Gowtham</a>
+        Built By 	&nbsp; <a href="https://github.com/Gowtz" className="text-blue-500"> @Gowtham</a>
     </nav>
   </footer>
   </>
