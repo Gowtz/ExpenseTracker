718971fec5c0167dfebb3d6f588272a7e9cbe667
Gowtham <gajavijay4@gmail.com>
2024-06-15 12:04:38 +0530

    another Typescript array bug in history is fixed and tested

--- src/Components/History.tsx
+++ src/Components/History.tsx
@@ -1,19 +1,17 @@
+// import transaction from "../Types/types";
+import transaction from "../Types/types";
 import Transaction from "./Transaction";
 
-export default function History({history}:{history:number[]}) {
-  return (
-  <>
-  <div className="history my-7">
-    <h1 className="font-semibold text-3xl mb-10">History</h1>
-    <ul>
-      {
-        history.map(tra => <Transaction tra={tra}/>)
-      }
-        {/* <li className="flex w-full h-10 rounded bg-slate-100 justify-between items-center px-10 my-1"><span className="note">Food</span> <span className="amount text-red-500">-$4.00</span></li>
-        <li className="flex w-full h-10 rounded bg-slate-100 justify-between items-center px-10 my-1"><span className="note">Dad</span> <span className="amount text-green-500">+$69.00</span></li> */}
+export default function History({ history }: { history: transaction[] }) {
 
-    </ul>
-  </div>
-  </>
+  return (
+    <>
+      <div className="history my-7">
+        <h1 className="font-semibold text-3xl mb-10">History</h1>
+        <ul>
+          {Array.isArray(history) && history?.map((tra) => <Transaction key={tra.id} tra={tra} />)}
+        </ul>
+      </div>
+    </>
   )
 }
--- src/Components/Transaction.tsx
+++ src/Components/Transaction.tsx
@@ -2,6 +2,6 @@
 
 export default function Transaction({tra}:any) {
   return (
-        <li key={tra.id} className="flex w-full h-10 rounded bg-slate-100 justify-between items-center px-10 my-1"><span className="note">{tra.message}</span> <span className={"amount "+(tra.type == "expense"?"text-red-500":"text-green-500")}>{(tra.type == "expense"?"-":"+" )}${tra.amount}</span></li>
+        <li  className="flex w-full h-10 rounded bg-slate-100 justify-between items-center px-10 my-1"><span className="note">{tra.message}</span> <span className={"amount "+(tra.type == "expense"?"text-red-500":"text-green-500")}>{(tra.type == "expense"?"-":"+" )}${tra.amount}</span></li>
   )
 }
