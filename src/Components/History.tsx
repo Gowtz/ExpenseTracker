fe34f7f604070ea46ffdd720bc8e9df5b152c2e6
Gowtham <gajavijay4@gmail.com>
2024-06-15 13:36:13 +0530

    UI update

--- src/Components/History.tsx
+++ src/Components/History.tsx
@@ -9,7 +9,7 @@ export default function History({ history }: { history: transaction[] }) {
       <div className="history my-7">
         <h1 className="font-semibold text-3xl mb-10">History</h1>
         <ul>
-          {Array.isArray(history) && history?.map((tra) => <Transaction key={tra.id} tra={tra} />)}
+          {Array.isArray(history) && history?.reverse().map((tra) => <Transaction key={tra.id} tra={tra} />)}
         </ul>
       </div>
     </>
--- src/Components/Transaction.tsx
+++ src/Components/Transaction.tsx
@@ -2,6 +2,6 @@
 
 export default function Transaction({tra}:any) {
   return (
-        <li  className="flex w-full h-10 rounded bg-slate-100 justify-between items-center px-10 my-1"><span className="note">{tra.message}</span> <span className={"amount "+(tra.type == "expense"?"text-red-500":"text-green-500")}>{(tra.type == "expense"?"-":"+" )}${tra.amount}</span></li>
+        <li  className={"flex w-full h-10 rounded bg-slate-100 justify-between items-center px-10 my-1 border border-y-0 border-r-0 border-l-4 " + (tra.type == "expense"? " border-red-500":"border-green-600")}><span className="note">{tra.message}</span> <span className={"amount "+(tra.type == "expense"?"text-red-500":"text-green-500")}>{(tra.type == "expense"?"-":"+" )}${tra.amount}</span></li>
   )
 }
